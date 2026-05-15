a5ce7c115cbd0364d45b76060936de2add5f7147
BoyCanad <boycanadian565@gmail.com>
2026-05-15 17:39:30 +0800

    update

--- stream/src/pages/SignUp.tsx
+++ stream/src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Check, CheckCircle2 } from 'lucide-react';
 import { supabase } from '../supabaseClient';
